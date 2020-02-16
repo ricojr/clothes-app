c0d6c6b7dd62a5dd4524c222ac38ea36c73957be
ricojr <ricopehangko@gmail.com>
2020-02-16 20:27:04 +0100

    :P

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/dir/dir.component'
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage;
