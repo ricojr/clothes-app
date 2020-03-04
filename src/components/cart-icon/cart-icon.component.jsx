4962098ee5332f8c4f9de57cc2a11be080d919fa
ricojr <ricopehangko@gmail.com>
2020-03-04 16:53:04 +0100

    added checkout page and modified cart and users selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
+
+import { createStructuredSelector } from 'reselect';
  
 import { toggleCartHidden } from '../../redux/cart/cart-actions';
+
+import { selectCartItemsCount } from '../../redux/cart/cart-selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
@@ -19,8 +23,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () =>dispatch(toggleCartHidden())
 });
 
-const mapToStateProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+const mapToStateProps = createStructuredSelector ({
+    itemCount: selectCartItemsCount
 })
 
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { auth } from '../../firebase/firebase.utils'
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import CartIcon from '../cart-icon/cart-icon.component'
+import { createStructuredSelector } from 'reselect';
+import { auth } from '../../firebase/firebase.utils';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { selectCartHidden } from '../../redux/cart/cart-selectors';
+import { selectCurrentUser } from '../../redux/user/user-selectors';
+import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
@@ -20,23 +23,18 @@ const Header = ({ currentUser, hidden}) => (
             <Link className='option' to="/shop">
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>
-                    SIGN IN
-                </Link>
-            }
+            
+            
+            {currentUser  ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>):(<Link className='option' to="/signin">SIGN IN</Link>)}
             <CartIcon />
         </div>
         {hidden ? null : <CartDropDown />}
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 }
 )
 
